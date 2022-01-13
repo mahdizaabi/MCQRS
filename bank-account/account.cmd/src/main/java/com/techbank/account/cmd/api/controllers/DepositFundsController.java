2a63622e2e86b4259c66cfa0c188db54ad1d7443
chiheblahouli <mahdi.zaabi@gmail.com>
2022-01-13 01:53:15 +0100

    Deposit funds controller + dispatch the corresponding commands

--- bank-account/account.cmd/src/main/java/com/techbank/account/cmd/api/controllers/DepositFundsController.java
+++ bank-account/account.cmd/src/main/java/com/techbank/account/cmd/api/controllers/DepositFundsController.java
@@ -1,48 +1,46 @@
 package com.techbank.account.cmd.api.controllers;
 
-
-import com.techbank.account.cmd.api.commands.OpenAccountCommand;
+import com.techbank.account.cmd.api.commands.DepositFundsCommand;
 import com.techbank.account.cmd.dto.OpenAccountResponse;
 import com.techbank.account.common.dto.BaseResponse;
 import com.techbank.cqrs.core.infrastructure.CommandDispatcher;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.text.MessageFormat;
-import java.util.UUID;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 @RestController
-@RequestMapping(path = "/api/v1/openbankaccount")
-public class OpenAccountContoller {
-    private final Logger logger = Logger.getLogger(OpenAccountContoller.class.getName());
+@RequestMapping(path = "/api/v1/depositfunds")
+public class DepositFundsController {
     @Autowired
     private CommandDispatcher commandDispatcher;
+    private final org.slf4j.Logger logger = LoggerFactory.getLogger(DepositFundsController.class);
 
-    @PostMapping
-    public ResponseEntity<BaseResponse> openAccount(@RequestBody OpenAccountCommand openAccountCommand) {
-        var SUCCESSFUL_MESSAGE_ACCOUNT_CREATION = "Bank account cretion request completed successfully!";
-        var id = UUID.randomUUID().toString();
-        openAccountCommand.setIdentidier(id);
+    @PutMapping(path = "/{id}")
+    public ResponseEntity<BaseResponse> depositFund(@PathVariable(value = "id") String id,
+                                                    @RequestBody DepositFundsCommand command) {
+        var SUCCESSFUL_MESSAGE_FUND_DEPOSIT = "Deposit found request completed successfully!";
+
         try {
-            commandDispatcher.send(openAccountCommand);
+            command.setIdentidier(id);
+            commandDispatcher.send(command);
+            return new ResponseEntity<BaseResponse>(new OpenAccountResponse(SUCCESSFUL_MESSAGE_FUND_DEPOSIT, id), HttpStatus.OK);
 
-            return new ResponseEntity<BaseResponse>(new OpenAccountResponse(SUCCESSFUL_MESSAGE_ACCOUNT_CREATION, id), HttpStatus.CREATED);
         } catch (IllegalStateException exc) {
             exc.printStackTrace();
-            logger.log(Level.WARNING, MessageFormat.format("client made a bad request {}", exc.toString()));
+            logger.error(exc.toString());
             return ResponseEntity.badRequest().body(new OpenAccountResponse(exc.toString()));
         } catch (Exception exc) {
             var safeMessageError = "Error while processin request to open a new bank account for id: " + id;
-            logger.warning(safeMessageError);
+            logger.error(safeMessageError);
             return new ResponseEntity<>(new BaseResponse(safeMessageError), HttpStatus.INTERNAL_SERVER_ERROR);
 
         }
 
     }
-
-
 }
